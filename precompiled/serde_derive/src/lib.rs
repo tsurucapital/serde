45a3539b1ed5104a857f4512381eaf8cb7b736eb
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2023-08-08 12:52:50 +0900

    Do not use precompiled serde_derive binary

--- precompiled/serde_derive/src/lib.rs
+++ precompiled/serde_derive/src/lib.rs
@@ -15,8 +15,4 @@
 
 #![doc(html_root_url = "https://docs.rs/serde_derive/1.0.181")]
 
-#[cfg(not(all(target_arch = "x86_64", target_os = "linux", target_env = "gnu")))]
 include!("lib_from_source.rs");
-
-#[cfg(all(target_arch = "x86_64", target_os = "linux", target_env = "gnu"))]
-include!("lib_precompiled.rs");
